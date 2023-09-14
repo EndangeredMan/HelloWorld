1b59a0f18e9981fa1844b8e3bd083c54149cb187
Eman <emanferrer52@gmail.com>
2023-09-14 11:35:20 -0500

    Update

--- app/src/main/java/com/cs407/helloworld/MainActivity.java
+++ app/src/main/java/com/cs407/helloworld/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView imgview
+    ImageView imgview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
